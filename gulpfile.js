71851d57cc21c1c62b31f17bfee153af60c0d9f1
hossamzee <hossam_zee@yahoo.com>
2015-08-12 13:09:00 +0300

    Ionic platform is almost ready.

--- gulpfile.js
+++ gulpfile.js
@@ -18,12 +18,12 @@ gulp.task('sass', function(done) {
     .pipe(sass({
       errLogToConsole: true
     }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./www/views/stylesheets/'))
     .pipe(minifyCss({
       keepSpecialComments: 0
     }))
     .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
+    .pipe(gulp.dest('./www/views/stylesheets/'))
     .on('end', done);
 });
 
